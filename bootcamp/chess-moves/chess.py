11c4ee1a22a4feaa2c4e2033d18d23031b5fcf7f
ngwanatuka <ngwanatuka@gmail.com>
2026-07-24 09:28:57 +0200

    Add the implementation of the parse_fen function

--- bootcamp/chess-moves/chess.py
+++ bootcamp/chess-moves/chess.py
@@ -1,15 +1,24 @@
 def parse_fen(fen):
-    fen_pieces, to_move, castling_rights, ep, hm, fm = fen.split(" ")
-    pieces = [[]]
+    # List unpacking. Unpacking the elements from the list and storing them in individual varoable
+    fen_pieces, to_move, castling_rights, ep, hm, fm = fen.split(" ") 
+   
+    # This our data model. A list of lists. Each inner list represents a row of the chess board, so we have 8 inner lists
+    pieces = [[]] 
+    
+    # This logic creates the chess board
     for char in fen:
         if char.isdigit():
-            pieces[-1].extend(["."] * int(char))
+            pieces[-1].extend(["*"] * int(char))
         elif char == "/":
             pieces.append([])
         else:
+            # Place the pieces on the board
             pieces[-1].append(char)
+        
+    return fen_pieces, to_move, castling_rights, ep, int(hm), int(fm), pieces
 
-    return ...
+# board = parse_fen("rnbqkbnr/pppppppp/8/8/8/5N2/PPPPPPPP/RNBQKB1R b KQkq - 1 1")[-1]
+# print(board)
 
 
 def generate_moves(board):
